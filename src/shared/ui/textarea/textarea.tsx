baef1b14be40bba1d8ea7f4cd7d9d16a6c436dc7
Damian Werens <dwerens@beesafe.pl>
2025-06-21 09:46:45 +0000

    :lipstick: ui(textarea.tsx): remove placeholder color while focusing and hovering.

--- src/shared/ui/textarea/textarea.tsx
+++ src/shared/ui/textarea/textarea.tsx
@@ -7,6 +7,8 @@ type ErrorValidation = { error?: string };
 type TTextarea = ComponentProps<"textarea"> &
   ErrorValidation & { labelText?: string };
 
+const TRANSITION_ARGS = [-1, 1.3, 0];
+
 const Textarea = forwardRef<HTMLTextAreaElement, TTextarea>(function Textarea(
   { className, error, id, labelText, ...props },
   ref,
@@ -15,8 +17,14 @@ const Textarea = forwardRef<HTMLTextAreaElement, TTextarea>(function Textarea(
     <Motion
       animate={
         error
-          ? { rotate: [-1, 1.3, 0], translateX: [-1, 1.3, 0] }
-          : { rotate: 0, translateX: 0 }
+          ? {
+              rotate: TRANSITION_ARGS,
+              translateX: TRANSITION_ARGS,
+            }
+          : {
+              rotate: 0,
+              translateX: 0,
+            }
       }
       transition={{ duration: 0.2 }}
       className="w-full"
@@ -32,7 +40,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, TTextarea>(function Textarea(
 
         <textarea
           className={clsx(
-            "min-h-64 w-full rounded-[10px] bg-zinc-100 px-3 py-2.5 text-sm text-zinc-900 transition-colors duration-200 placeholder:text-zinc-400 hover:bg-zinc-200 hover:text-zinc-500 focus-visible:bg-zinc-200 focus-visible:text-zinc-500 dark:bg-zinc-900 dark:text-zinc-100 dark:placeholder:text-zinc-600 dark:hover:bg-zinc-800 dark:hover:placeholder:text-zinc-500 dark:focus-visible:bg-zinc-800 dark:focus-visible:placeholder:text-zinc-500",
+            "min-h-64 w-full rounded-[10px] bg-zinc-100 px-3 py-2.5 text-sm text-zinc-900 transition-colors duration-200 placeholder:text-zinc-400 hover:bg-zinc-200 focus-visible:bg-zinc-200 dark:bg-zinc-900 dark:text-zinc-100 dark:placeholder:text-zinc-600 dark:hover:bg-zinc-800 dark:focus-visible:bg-zinc-800",
             error && "ring-2 ring-rose-500 focus-visible:!outline-none",
             className,
           )}
