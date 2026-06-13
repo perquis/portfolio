e327852a3aa778fcb9f2b0cb43964c14baf94b26
Damian Werens <thetahirpl@gmail.com>
2026-06-13 19:48:30 +0200

    :adhesive_bandage: fix(metadata): Add new field metadataBase to config.

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,10 +1,15 @@
+import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
 
+import { BASE_URL } from "@/common/constants/env";
 import { locales } from "@/libs/next-intl";
 
 const [en, pl] = locales;
 
-export const metadata = {
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    BASE_URL?.startsWith("http") ? BASE_URL : `https://${BASE_URL}`,
+  ),
   alternates: {
     canonical: "/",
     languages: {
